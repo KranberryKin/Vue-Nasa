c47e41ee7759132a1fe7fd96efcf82585f196f75
Quinnton <quinnclimbs@gmail.com>
2021-09-21 14:24:11 -0600

    commit

---
+++ src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-berrykin.us.auth0.com'
+export const audience = 'Basic.com'
+export const clientId = 'dbJHBa7ZSZkpZRLYlmMDlabmfAEomj28'
